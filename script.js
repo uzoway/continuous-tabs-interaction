63029aaaacc54bd850ae9f973d00c4ec0ad19cb3
Uzochukwu Okafor <uzochukwuokafor01@gmail.com>
2024-11-27 04:28:44 +0100

    feat: Add the continuous tab interaction that respects the prefers-reduce-motion media

--- script.js
+++ script.js
@@ -3,6 +3,51 @@
 gsap.registerPlugin(CustomEase);
 CustomEase.create("ease-out-cubic", "0.215,0.61,0.355,1");
 
-function initializeAnimation() {}
+const navBtns = gsap.utils.toArray("[data-nav-btn]");
+let currentNavbutton = navBtns[0];
+const tabIndicator = document.querySelector("[data-tab-indicator]");
 
-window.addEventListener("DOMContentLoaded", initializeAnimation);
+const updateTabIndicator = () => {
+  gsap.set(tabIndicator, {
+    x: currentNavbutton.offsetLeft,
+    width: currentNavbutton.clientWidth,
+  });
+};
+
+const mm = gsap.matchMedia();
+
+mm.add(
+  {
+    isDesktop: "(min-width: 750px)",
+    reduceMotion: "(prefers-reduced-motion: reduce)",
+  },
+  (context) => {
+    const { reduceMotion } = context.conditions;
+
+    navBtns.forEach((button) => {
+      button.addEventListener("click", function () {
+        const prefersReducedMotion = window.matchMedia(
+          "(prefers-reduced-motion: reduce)"
+        ).matches;
+
+        gsap.to(tabIndicator, {
+          x: button.offsetLeft,
+          width: button.clientWidth,
+          overwrite: true,
+          ease: prefersReducedMotion ? "none" : "back.out(.8)",
+          duration: prefersReducedMotion ? 0 : 0.51,
+        });
+
+        currentNavbutton = button;
+      });
+    });
+  }
+);
+
+// Update the tab indicator on window resize
+window.addEventListener("resize", () => {
+  updateTabIndicator();
+});
+
+// Initialize the tab indicator on page load
+updateTabIndicator();
